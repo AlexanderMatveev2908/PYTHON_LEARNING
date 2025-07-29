42be50fb4cf79286b0abe7e75223d6fcf93e1847
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2025-07-29 09:34:20 +0300

    main => _

---
+++ 23-venv/src/script.py
@@ -0,0 +1,33 @@
+import asyncio
+from typing import Dict
+import httpx
+
+api = httpx.AsyncClient(base_url="https://jsonplaceholder.typicode.com")
+
+
+async def get_posts() -> list[Dict]:
+    res = await api.get("/posts")
+    res = res.json()
+    return res
+
+
+async def get_users() -> list[Dict]:
+    res = await api.get("/users")
+    res = res.json()
+    return res
+
+
+# posts = asyncio.run(get_posts())
+# for i in range(10):
+#     print(posts[i])
+
+
+async def main() -> None:
+    users, posts = await asyncio.gather(get_users(), get_posts())
+
+    for data in [users, posts]:
+        for i in range(5):
+            print(data[i])
+
+
+asyncio.run(main())
